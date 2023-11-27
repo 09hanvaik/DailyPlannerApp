a44aa0818b9492315d776e121c37195f0ab0582e
Harry Vaikkath <hvaikkath20@gmail.com>
2023-11-27 07:32:17 +0000

    added jQuery elements for day calendar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // Variables
 var currentDay = $("#currentDay");
+var timeblocksEl = $("#timeSlots");
 
 
 //Function to find the suffix of the day
@@ -22,3 +23,74 @@ var daySuffix = nthNumber(dayjs().format("D"));
 var now = dayjs().format("dddd, MMMM D");
 currentDay.text(now+daySuffix);
 
+// Time Slots
+var timeSlots = [
+    "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"
+];
+
+function createTimeblock() {
+    for (var i = 0; i < timeSlots.length; i++) {
+        var side = timeSlots[i];
+
+        var row = $("<div>").addClass("row");
+        timeblocksEl.append(row);
+
+        var hour = $("<div>").addClass("hour block");
+        hour.html(side);
+        row.append(hour);
+
+        var text = $("<textarea>").addClass("description block").attr('id', i);
+        row.append(text);
+
+        var save = $("<button>").addClass("saveBtn block").attr("value", i);
+        save.text("Save");
+        row.append(save);
+    }
+}
+
+createTimeblock();
+
+// Coloured time slots
+function colouredTimeblocks() {
+    var GetCurrentHR = dayjs().format('h a');
+    var CurrentHour = dayjs(GetCurrentHR, 'h a');
+    var Descriptions = $('.description'); // Using jQuery to select elements
+    
+    Descriptions.each(function (i) {
+        var TimeBlock = dayjs(timeSlots[i], 'h a');
+        if (CurrentHour.isSame(TimeBlock)) {
+            $(this).addClass('present').removeClass('future past');
+        } else if (CurrentHour.isBefore(TimeBlock)) {
+            $(this).addClass('future').removeClass('past present');
+        } else if (CurrentHour.isBefore(TimeBlock) === false) {
+            $(this).addClass('past').removeClass('future present');
+        }
+    });
+}
+
+$(document).ready(function () {
+    colouredTimeblocks();
+    setInterval(colouredTimeblocks, 10000);
+});
+
+
+// Save function
+$(document).on('click', '.saveBtn', function (event) {
+    event.preventDefault();
+    var SaveBtnValue = $(this).val();
+    var description = document.getElementById(SaveBtnValue).value;
+    localStorage.setItem(SaveBtnValue, description);
+});
+
+// Get stored notes
+function storedNotes() {
+    for (var i = 0; i < timeSlots.length; i++) {
+        var getStoredNotes = localStorage.getItem(i);
+        var text = document.getElementById(i);
+        // if (getStoredNotes !== null) {
+        //     text.append(getStoredNotes);
+        // }
+    }
+}
+
+storedNotes();
